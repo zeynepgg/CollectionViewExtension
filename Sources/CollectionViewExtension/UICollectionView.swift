66ccf4d9aaab2f3f49f771d8eb055e006b71e259
zeynepgg <zeynepgursoy@outlook.com>
2022-04-06 15:51:22 +0300

    add public funcs

--- Sources/CollectionViewExtension/UICollectionView.swift
+++ Sources/CollectionViewExtension/UICollectionView.swift
@@ -9,11 +9,11 @@ import Foundation
 import UIKit
 
 extension UICollectionView {
-    func register(cellType: UICollectionViewCell.Type) {
+   public func register(cellType: UICollectionViewCell.Type) {
         register(cellType.nib, forCellWithReuseIdentifier: cellType.identifier)
     }
     
-    func dequeCell<T: UICollectionViewCell>(cellType: T.Type, indexPath: IndexPath) -> T {
+    public func dequeCell<T: UICollectionViewCell>(cellType: T.Type, indexPath: IndexPath) -> T {
         guard let cell = dequeueReusableCell(withReuseIdentifier: cellType.identifier, for: indexPath) as? T else {
             fatalError("Hata meydana geldi")
         }
--- Sources/CollectionViewExtension/UICollectionViewCell.swift
+++ Sources/CollectionViewExtension/UICollectionViewCell.swift
@@ -9,10 +9,10 @@ import Foundation
 import UIKit
 
 extension UICollectionViewCell {
-    static var identifier: String {
+   public static var identifier: String {
         return String(describing: self)
     }
-    static var nib: UINib {
+    public static var nib: UINib {
         return UINib(nibName: identifier, bundle: nil)
     }
 }
